dfbe6c9f8118c65c0608355ba902bcf8cbcca03d
Cameron Hudson <camnn3@yahoo.com>
2020-07-16 16:22:03 +0000

    Automatically backed up by Learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -2,7 +2,7 @@ require_relative './class_methods_module.rb'
 require_relative './dance_module.rb'
 require_relative './fancy_dance.rb'
 
-class Kid 
+class Dancer
   extend FancyDance::ClassMethods
   include FancyDance::InstanceMethods
   
